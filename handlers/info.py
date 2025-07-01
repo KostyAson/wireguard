e80f9160aad5aaf7ff1a0aa08ef47bd4b1aba2ee
KostyAson <kostya.ason@bk.ru>
2025-07-02 02:19:06 +0300

    Изменил стартовое сообщение

--- handlers/info.py
+++ handlers/info.py
@@ -4,6 +4,7 @@ import answers
 import utils
 import log
 import datetime as dt
+import os
 
 router = aiogram.Router()
 
@@ -36,6 +37,20 @@ async def start_message(message : aiogram.types.Message, command : aiogram.filte
         await message.answer(text=answers.start_with_sub.replace('{days}', str(days)), parse_mode='HTML', disable_web_page_preview=True)
         utils.set_user_use_free_sub(message.from_user.id)
         utils.set_user_subscription(message.from_user.id, 1, (dt.datetime.now() + dt.timedelta(days=days)).isoformat())
+
+        #  добавление устройства "start"
+        utils.add_device(message.from_user.id, "start", name)
+        normal_name = utils.get_normal_device_name(message.text)
+        os.system(f'qrencode -t png -s 10 -m 1 -o qr.png < {normal_name}.conf')
+        await message.answer_document(
+            aiogram.types.input_file.FSInputFile(f'{normal_name}.conf'),
+            caption=f'Файл для подключения к VPN'
+        )
+        await message.answer_photo(
+            photo=aiogram.types.FSInputFile(f'qr.png'),
+            caption=f'QR для подключения к VPN'
+        )
+        os.system(f'rm "{normal_name}.conf" && rm "qr.png"')
     else:
         await message.answer(text=answers.start, parse_mode='HTML', disable_web_page_preview=True)
 
