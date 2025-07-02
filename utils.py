9f686df87ab75a5161007aa28b3526acf705e643
KostyAson <kostya.ason@bk.ru>
2025-07-02 04:09:53 +0300

    исправил баг

--- utils.py
+++ utils.py
@@ -358,7 +358,7 @@ def get_ad_users(id):
             last = line.split()[1]
         elif line.startswith('  endpoint'):
             cur.execute(f'SELECT user_id FROM devices WHERE public_key="{last}";')
-            user = cur.fetchone[0]
+            user = cur.fetchone()[0]
             cur.execute(f'SELECT from_ad FROM users WHERE id={user};')
             if cur.fetchone()[0] == id:
                 users.add(user)
